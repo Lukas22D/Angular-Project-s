cb713da3c3b32cc9a8904a03fc2769f9170d3343
Lukas22D <luk.look.s.rocha@gmail.com>
2022-05-12 14:26:34 -0300

    Segragação de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,15 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { CourseListComponent } from './courses/course-list.component';
-import { starComponent } from './star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './error-404/error-404.component';
-import { CourseInfoComponent } from './courses/course-info.component';
+import { CourseModule } from './courses/curse.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    starComponent,
-    ReplacePipe,
     NavBarComponent,
     Error404Component,
-    CourseInfoComponent
     
     
   ],
@@ -28,13 +21,8 @@ import { CourseInfoComponent } from './courses/course-info.component';
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    CourseModule,
     RouterModule.forRoot([
-      {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        path: 'courses/info/:id', component: CourseInfoComponent
-      },
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
       },
